50d65b887d0a9e52f6e5256237effa1e02508fd8
David DE JESUS DUARTE <dde-jesu@student.42.fr>
2018-11-14 12:45:48 +0100

    Change BUFF_SIZE to macos Page Size

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: dde-jesu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 16:09:53 by dde-jesu          #+#    #+#             */
-/*   Updated: 2018/11/14 09:42:10 by dde-jesu         ###   ########.fr       */
+/*   Updated: 2018/11/14 11:59:44 by dde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 5
+# define BUFF_SIZE 4096
 
 int				get_next_line(const int fd, char **line);
 
